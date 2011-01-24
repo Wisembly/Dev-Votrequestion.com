6e07a06e5a52c4175e16cee8ede52c37b6ec1c18
Guillaume Potier <cobalt2760@gmail.com>
2011-01-24 20:45:24 +0100

    Création d'un csv pour export excel du listing AG

--- balloon_presencelist/test.php
+++ balloon_presencelist/test.php
@@ -1,23 +1,76 @@
 <?php
-$bi = array("super;star",'coucou;','cocu');
+die();
+include 'common.php';
+?>
 
-foreach($bi as $what){
-$test = explode(";",$what) ;
+	<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+	<html class="ui-mobile portrait min-width-320px min-width-480px max-width-768px max-width-1024px">
+	  <head>
+	    <title>Index - PresenceList</title>
+	    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	  </head>
+	  <body class="ui-mobile-viewport">
 
-if ( sizeof($test) == 1 || $test[1] == '' )
+<?php
+ob_start();
+	echo "Nom;Prénom;Type d'Invités;Société;Invitant;Présence;Heure;Commentaires;Vip;Modif;\n";
+$select = mysql_query("SELECT * FROM presencelist_user AS U LEFT OUTER JOIN presencelist_action AS A ON U.id = A.user_id WHERE U.presencelist_id='1' GROUP BY U.id ORDER BY U.nom ASC, U.prenom ASC");
+while ( $s = mysql_fetch_array($select) )
 {
-	if( sizeof($test) > 1 )	$what = substr($what,0,strpos(';')-1);
-	$sql = "SELECT id,nom,prenom FROM pouet WHERE `nom` LIKE  '%$what%' OR `prenom` LIKE  '%$what%' AND has_checked = '0' ORDER BY nom ASC";
+	// traitement other_info
+	$replace = array("<b>Type d'Invité</b> : ","<b>Type d\'Invité</b> : ","<b>Société</b> : ","<b>Invitant</b> : ","<b>Commentaires</b> : ","<b>VIP</b> : ","<b>Modif</b> : ");
+	$other = str_replace($replace,"",$s['other_info']);
+	$o = explode("<br/>",$other);
+	
+	echo strtoupper($s['nom']);	
+	echo ';';
+	echo ucfirst(strtolower($s['prenom']));
+	echo ';';	
+	echo $o[0] ;
+	echo ';';	
+	echo $o[1] ;
+	echo ';';	
+	echo $o[2] ;
+	echo ';';	
+	echo ($s['has_checked'] == 1) ? 'OUI' : 'NON';
+	echo ';';
+	echo ($s['time'] != "") ? custom_date($s['time']) : "";
+	echo ';';
+	echo ($s['type'] == 2) ? 'CRÉÉ SUR PLACE '.$o[3] : ''.$o[3] ;
+	echo ';';	
+	echo $o[4] ;
+	echo ';';
+	echo $o[5];
+	echo ";\n";		
 }
-else
-{
-	$first = $test[0];
-	$second = $test[1];
+	$content = ob_get_contents();
+	ob_end_clean();
+	
+	$f = fopen('export.csv','w');
+	fwrite($f,$content);
+	if ( fclose($f) )
+		echo 'Correctement Exporté';
+	else
+		echo 'Erreur';
+?>
 
-	$sql = "SELECT id,nom,prenom FROM poeut WHERE `nom` LIKE  '%$first%' OR `prenom` LIKE  '%$first%' OR `nom` LIKE  '%$second%' OR `prenom` LIKE  '%$second%' AND has_checked = '0' ORDER BY nom ASC";
+</body>
+</html>
+
+<?php
+function custom_date ($date,$short = 0) {
+	$mois = array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre' );
+	if ( $short == 0 )
+	 	return date('d' ,$date).' '.$mois[date('n' ,$date)-1].' '.date('Y' ,$date).' à '.date('G\hi' ,$date) ;
+	else if ( $short == 1 )
+		return date('d' ,$date).' '.$mois[date('n' ,$date)-1].' '.date('Y' ,$date) ;
+	else if ( $short == 2 )
+		return date('d' ,$date).'/'.date('m' ,$date).'/'.date('Y' ,$date).' à '.date('G\hi' ,$date) ;
+	else
+		return date('d' ,$date).' '.$mois[date('n' ,$date)-1].' '.date('Y' ,$date) ;
+		
+	 	
+	// date('g:i a' ,$date) //pour 15:45pm
 }
 
-	echo $sql.'<br/><br/>';
-
-}
 ?>
