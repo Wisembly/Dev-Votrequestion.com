c2cfd80fb55245830673a822004b9f6860869192
Guillaume Potier <cobalt2760@gmail.com>
2011-03-04 18:18:56 +0100

    Include sql class

--- rms/inc/config.php
+++ rms/inc/config.php
@@ -1,7 +1,25 @@
 <?php
 
-mysql_connect('10.0.211.162', 'rms', 'ball00nSXSW2011');
-mysql_select_db('rms');
+require_once 'db/mysql.php' ;
+
+$db_host = '10.0.211.162';
+$db_user = 'rms';
+$db_pass = 'ball00nSXSW2011';
+$db_name = 'rms';
+$db_port = '';
 $table_prefix = 'ratemyspeaker_';
 
-?>
+//Construct a db instance
+	$db = new $sql_db();
+	
+	// essai connexion à SQL Privé
+	if(is_array($db->sql_connect(
+							$db_host, 
+							$db_user,
+							$db_pass,
+							$db_name, 
+							$db_port,
+							false, 
+							false
+	))) 
+		die("Unable to connect to the database");
