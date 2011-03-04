072afefda9a4d66cb624b82179f25672da8da789
Guillaume Potier <cobalt2760@gmail.com>
2011-03-04 20:23:35 +0100

    autocomplete2 + css

--- rms/ajax/search.ajax.php
+++ rms/ajax/search.ajax.php
@@ -4,9 +4,9 @@ require_once '../inc/config.php';
 
 $search = $_GET['q'];
 
-$speakers = mysql_query("SELECT id, real_name FROM ".$table_prefix."Speaker WHERE real_name LIKE '%".$search."%' LIMIT 10");
+$speakers = mysql_query("SELECT id, real_name, url_avatar FROM ".$table_prefix."Speaker WHERE real_name LIKE '%".$search."%' LIMIT 5");
 
 while ($speaker = mysql_fetch_row($speakers))
-	echo $speaker[1].'|'.$speaker[0]."\n";
+	echo $speaker[1].'..'.$speaker[2].'|'.$speaker[0]."\n";
 	
 ?>
