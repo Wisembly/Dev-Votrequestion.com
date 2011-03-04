8e6934a8495b9ec7d42a75418aa53f53ec99b4a8
Guillaume Potier <cobalt2760@gmail.com>
2011-03-04 18:27:21 +0100

    Remodif sur appel de base.

--- rms/inc/config.php
+++ rms/inc/config.php
@@ -10,16 +10,5 @@ $db_port = '';
 $table_prefix = 'ratemyspeaker_';
 
 //Construct a db instance
-	$db = new $sql_db();
-	
-	// essai connexion à SQL Privé
-	if(is_array($db->sql_connect(
-							$db_host, 
-							$db_user,
-							$db_pass,
-							$db_name, 
-							$db_port,
-							false, 
-							false
-	))) 
-		die("Unable to connect to the database");
+mysql_connect($db_host,$db_user,$db_pass);
+mysql_select_db($db_name);
