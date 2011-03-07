4d80525e3fc31e2fe2eccf19e8e4e422a7f652b3
Guillaume Potier <cobalt2760@gmail.com>
2011-03-08 00:01:55 +0100

    Mise en place d'une 404

---
+++ rms/inc/404.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'config.php';
+
+$title = 'RateMySpeaker';
+$description = null;
+$keywords = null;
+
+include 'header.php';
+?>
+
+
+<div id="four0four"><strong>404</strong><br/>page not found..</div>
+
+<?php
+
+include 'footer.php';
+
+?>
