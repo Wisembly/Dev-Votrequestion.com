a1362006a43e2011d17f75c55954b2a917f5be05
Romain David <romain@balloonup.com>
2011-03-07 11:18:47 +0100

    lien Facebook

--- rms/inc/footer.php
+++ rms/inc/footer.php
@@ -1,6 +1,6 @@
 		<div id="<?php echo empty($_GET['page']) ? 'home' : 'balloon'; ?>">
 			Powered by <a href="http://balloonup.com"><img class="logoballoon" src="img/logo/balloonlogo.png"></a>
-			<br/><small>Follow <a href="http://twitter.com/balloon">@balloon</a> on Twitter<br/>
+			<br/><small>Follow <a href="http://twitter.com/balloon">@balloon</a> on Twitter or fan us on <a href="http://facebook.com/balloonup">Facebook</a><br/>
 			This site is not endorsed or supported by SXSW, Inc.</small>
 		</div>
 	</div>
