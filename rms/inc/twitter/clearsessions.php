e8405ed754e77eb729ffe84f9b514beba63d3650
Guillaume Potier <cobalt2760@gmail.com>
2011-03-08 10:11:00 +0100

    Try logout avec suppr cookies

--- rms/inc/twitter/clearsessions.php
+++ rms/inc/twitter/clearsessions.php
@@ -7,6 +7,17 @@
 /* Load and clear sessions */
 session_start();
 session_destroy();
+
+/* Delete cookies */
+setcookie("ratemyspeaker[user_id]") ;
+setcookie("ratemyspeaker[pseudo_twitter_user]") ;
+setcookie("ratemyspeaker[url_avatar_user]") ;
+
+
+unset($_COOKIE['ratemyspeaker']['user_id']);
+unset($_COOKIE['ratemyspeaker']['pseudo_twitter_user']);
+unset($_COOKIE['ratemyspeaker']['pseudo_twitter_user']);
+
  
 /* Redirect to page with the connect to Twitter option. */
 header('Location: ../../index.php');
